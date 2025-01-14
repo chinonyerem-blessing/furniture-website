abba1dfd9f027c21514d850cfad0828926508332
chinonyerem-blessing <chinonyeremblessing95@gmail.com>
2025-01-14 13:08:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,478 @@
+window.addEventListener('scroll', () => {
+    const elementTen1 = document.querySelector(".navButton");
+    const rect = elementTen1.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen1.classList.add("animateTen1");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen2 = document.querySelector(".home");
+    const rect = elementTen2.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen2.classList.add("animateTen2");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen3 = document.querySelector(".body2 p");
+    const rect = elementTen3.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen3.classList.add("animateTen3");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen4 = document.querySelector(".icon");
+    const rect = elementTen4.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen4.classList.add("animateTen4");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen5 = document.querySelector(".box2pp");
+    const rect = elementTen5.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen5.classList.add("animateTen5");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen6 = document.querySelector("h1");
+    const rect = elementTen6.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen6.classList.add("animateTen6");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen7 = document.querySelector(".feature");
+    const rect = elementTen7.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen7.classList.add("animateTen7");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen8 = document.querySelector(".best");
+    const rect = elementTen8.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen8.classList.add("animateTen8");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen9 = document.querySelector(".box1");
+    const rect = elementTen9.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen9.classList.add("animateTen9");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen10 = document.querySelector(".box2p");
+    const rect = elementTen10.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen10.classList.add("animateTen10");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen11 = document.querySelector(".box2 img");
+    const rect = elementTen11.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen11.classList.add("animateTen11");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen12 = document.querySelector("box3");
+    const rect = elementTen12.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen12.classList.add("animateTen12");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen13 = document.querySelector(".box222");
+    const rect = elementTen13.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen13.classList.add("animateTen13");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen14 = document.querySelector(".furnimg1");
+    const rect = elementTen14.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen14.classList.add("animateTen14");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen15 = document.querySelector(".view");
+    const rect = elementTen15.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen15.classList.add("animateTen15");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen16 = document.querySelector(".closer");
+    const rect = elementTen16.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen16.classList.add("animateTen16");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen17 = document.querySelector(".video");
+    const rect = elementTen17.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen17.classList.add("animateTen17");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen18 = document.querySelector(".num");
+    const rect = elementTen18.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen18.classList.add("animateTen18");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen19 = document.querySelector(".img1 img");
+    const rect = elementTen19.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen19.classList.add("animateTen19");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen20 = document.querySelector(".fllat");
+    const rect = elementTen20.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen20.classList.add("animateTen20");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen21 = document.querySelector(".fllat1");
+    const rect = elementTen21.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen21.classList.add("animateTen21");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen22 = document.querySelector(".fllat2");
+    const rect = elementTen22.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen22.classList.add("animateTen22");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen23 = document.querySelector(".fllat3");
+    const rect = elementTen23.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen23.classList.add("animateTen23");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen24 = document.querySelector(".imgg img");
+    const rect = elementTen24.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen24.classList.add("animateTen24");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen25 = document.querySelector(".extra");
+    const rect = elementTen25.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen25.classList.add("animateTen25");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen26 = document.querySelector(".lorem");
+    const rect = elementTen26.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen26.classList.add("animateTen26");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen27 = document.querySelector(".box8");
+    const rect = elementTen27.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen27.classList.add("animateTen27");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen28 = document.querySelector(".pro");
+    const rect = elementTen28.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen28.classList.add("animateTen28");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen29 = document.querySelector(".vide");
+    const rect = elementTen29.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen29.classList.add("animateTen29");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen30 = document.querySelector(".img2");
+    const rect = elementTen30.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen30.classList.add("animateTen30");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen32 = document.querySelector(".modern");
+    const rect = elementTen31.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen31.classList.add("animateTen31");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen32 = document.querySelector(".mode");
+    const rect = elementTen32.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen32.classList.add("animateTen32");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen33 = document.querySelector(".visi button");
+    const rect = elementTen33.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen33.classList.add("animateTen33");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen34 = document.querySelector(".box9");
+    const rect = elementTen34.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen34.classList.add("animateTen34");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen35 = document.querySelector(".besti");
+    const rect = elementTen35.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen35.classList.add("animateTen35");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen36 = document.querySelector(".featurer");
+    const rect = elementTen36.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen36.classList.add("animateTen36");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen37 = document.querySelector(".img3");
+    const rect = elementTen37.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen37.classList.add("animateTen37");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen38 = document.querySelector(".vol");
+    const rect = elementTen38.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen38.classList.add("animateTen38");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen39 = document.querySelector(".num1");
+    const rect = elementTen39.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen39.classList.add("animateTen39");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen40 = document.querySelector(".num2");
+    const rect = elementTen40.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen40.classList.add("animateTen40");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen41 = document.querySelector(".box14");
+    const rect = elementTen41.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen41.classList.add("animateTen41");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen42 = document.querySelector(".naame");
+    const rect = elementTen42.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen42.classList.add("animateTen42");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen43 = document.querySelector(".texte");
+    const rect = elementTen43.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen43.classList.add("animateTen43");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen44 = document.querySelector(".textee");
+    const rect = elementTen44.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen44.classList.add("animateTen44");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen45 = document.querySelector(".sendd");
+    const rect = elementTen45.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen45.classList.add("animateTen45");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen47 = document.querySelector(".box91");
+    const rect = elementTen47.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen47.classList.add("animateTen47");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen48 = document.querySelector(".box92");
+    const rect = elementTen48.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen48.classList.add("animateTen48");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen49 = document.querySelector(".box93");
+    const rect = elementTen49.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen49.classList.add("animateTen49");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen50 = document.querySelector(".box94");
+    const rect = elementTen50.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen50.classList.add("animateTen50");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen51 = document.querySelector(".box95");
+    const rect = elementTen51.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen51.classList.add("animateTen51");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen52 = document.querySelector(".visi1 button");
+    const rect = elementTen52.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen52.classList.add("animateTen52");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const elementTen53 = document.querySelector(".visi2 button");
+    const rect = elementTen53.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen53.classList.add("animateTen53");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const elementTen54 = document.querySelector(".visi3 button");
+    const rect = elementTen54.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen54.classList.add("animateTen54");
+    }
+});
+
+window.addEventListener('scroll', () => {
+    const elementTen55 = document.querySelector(".visi4 button");
+    const rect = elementTen55.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen55.classList.add("animateTen55");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen56 = document.querySelector(".visi5 button");
+    const rect = elementTen56.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen56.classList.add("animateTen56");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen57 = document.querySelector(".mode1");
+    const rect = elementTen57.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen57.classList.add("animateTen57");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen58 = document.querySelector(".mode2");
+    const rect = elementTen58.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen58.classList.add("animateTen58");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen59 = document.querySelector(".mode3");
+    const rect = elementTen59.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen59.classList.add("animateTen59");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen60 = document.querySelector(".mode4");
+    const rect = elementTen60.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen60.classList.add("animateTen60");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen61 = document.querySelector(".mode5");
+    const rect = elementTen61.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen61.classList.add("animateTen61");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen62 = document.querySelector(".boxx3");
+    const rect = elementTen62.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen62.classList.add("animateTen62");
+    }
+});
+window.addEventListener('scroll', () => {
+    const elementTen63 = document.querySelector(".icon p");
+    const rect = elementTen63.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+        elementTen63.classList.add("animateTen63");
+    }
+});
+
+
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(link => {
+
+    link.addEventListener("click", event => {
+        event.preventDefault();
+
+        const sectionId = link.getAttribute("href");
+
+        const section = document.querySelector(sectionId);
+
+        section.scrollIntoView({ behaviour: "smooth" })
+    });
+});
+const navLink = document.querySelectorAll("dropdown-menu a");
+navLinks.forEach(link => {
+
+    link.addEventListener("click", event => {
+        event.preventDefault();
+
+        const sectionIds = link.getAttribute("href");
+
+        const sectioni = document.querySelector(sectionIds);
+
+        sectioni.scrollIntoView({ behaviour: "smooth" })
+    });
+});
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu")
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle("open")
+    const isOpen = dropDownMenu.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+        ? "fa-solid fa-xmark"
+        : "fa-solid fa-bars"
+}
